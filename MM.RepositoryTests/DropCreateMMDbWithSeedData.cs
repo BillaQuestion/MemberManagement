d20a52f7cb5176cfa4a2d353f02c8b330819e49f
RichardZZM <richardzzm@msn.com>
2017-02-05 21:35:30 +0800

    增加了Repository层的测试

---
+++ MM.RepositoryTests/DropCreateMMDbWithSeedData.cs
@@ -0,0 +1,33 @@
+﻿using MM.Model;
+using System.Data.Entity;
+
+namespace MM.Repository.Tests
+{
+    public class DropCreateMMDbWithSeedData : DropCreateDatabaseAlways<MMContext>
+    {
+        protected override void Seed(MMContext context)
+        {
+            base.Seed(context);
+
+            var oneTimeExperience = new OneTimeExperience()
+                { Name = "一次性画纸", Price = 20 };
+            oneTimeExperience.GenerateNewIdentity();
+            context.OneTimeExperiences.Add(oneTimeExperience);
+
+            oneTimeExperience = new Model.OneTimeExperience()
+            { Name = "一次性画布", Price = 30 };
+            oneTimeExperience.GenerateNewIdentity();
+            context.OneTimeExperiences.Add(oneTimeExperience);
+
+            var lecture = new Lecture()
+            {
+                Name = "水粉画基础",
+                Price = 998,
+                Count = 14,
+                Description = "从零开始学习水粉画的基本画法"
+            };
+            lecture.GenerateNewIdentity();
+            context.Lectures.Add(lecture);
+        }
+    }
+}
--- MM.RepositoryTests/MemberRepositoryTests.cs
+++ MM.RepositoryTests/MemberRepositoryTests.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MM.Model;
 using MM.Repository;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,26 @@ namespace MM.Repository.Tests
     [TestClass()]
     public class MemberRepositoryTests
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.SetInitializer(new DropCreateMMDbWithSeedData());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            var context = new MMContext();
+            context.Database.Delete();
+        }
+
         [TestMethod()]
-        public void CreateMMDb()
+        public void GetOneTimeExperienceTest()
         {
             var context = new MMContext();
             var productRepository = new ProductRepository(context);
             var products = productRepository.GetAll().ToList();
+            Assert.IsTrue(products.First() is OneTimeExperience);
         }
     }
 }
