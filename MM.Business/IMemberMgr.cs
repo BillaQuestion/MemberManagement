4210057a34fd6e9a8444b53aaaecc2af0cc98aa2
Bill@Surface <zhangshiyao_ZSY@outlook.com>
2017-02-17 12:00:15 +0800

    Add IMemberMgr, MemberMgr, MemberExistException

---
+++ MM.Business/IMemberMgr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MM.Model;
+
+namespace MM.Business
+{
+    public interface IMemberMgr
+    {
+        Member GetMember(Guid memberId);
+        IEnumerable<Member> GetAllMember();
+        void CreateMember(Member member);
+        void ModifyMember(Member member);
+        void RemoveMember(Guid memberId);
+    }
+}
---
+++ MM.Business/MemberMgr.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MM.Model.IRepositories;
+using MM.Model;
+using Dayi.Data.Domain.Seedwork.Specification;
+using MM.Business.Exceptions;
+
+namespace MM.Business
+{
+    public class MemberMgr : IMemberMgr
+    {
+        private IMemberRepository _memberRepository;
+
+        public MemberMgr(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        IEnumerable<Member> IMemberMgr.GetAllMember()
+        {
+            return _memberRepository.GetAll();
+        }
+
+        Member IMemberMgr.GetMember(Guid memberId)
+        {
+            return _memberRepository.GetByKey(memberId);
+        }
+
+        void IMemberMgr.CreateMember(Member member)
+        {
+            ISpecification<Member> spec = new DirectSpecification<Member>(m => m.PhoneNumber == member.PhoneNumber);
+            var result = _memberRepository.FindBySpecification(spec).FirstOrDefault();
+
+            if (result != null)
+                throw new MemberExistException();
+            else
+                _memberRepository.Add(member);
+        }
+
+        void IMemberMgr.ModifyMember(Member member)
+        {
+            var result = _memberRepository.GetByKey(member.Id);
+            if (result == null)
+                throw new MemberNotExistException();
+            else
+                _memberRepository.Modify(member);
+        }
+
+        void IMemberMgr.RemoveMember(Guid memberId)
+        {
+            var result = _memberRepository.GetByKey(memberId);
+            if (result == null)
+                throw new MemberNotExistException();
+            else
+                _memberRepository.Remove(result);
+        }
+    }
+}
